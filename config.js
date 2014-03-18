47cff58bc099b2a31b9a4e2bc38bd5367a8a0cf2
Craig Freeman <craig@crowdfavorite.com>
2014-03-17 19:59:52 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'eSqsKT95gbbmrtaCABn4lg',
+  consumer_secret: 'zAD0Jun9bDfNgzfeh3J83kNfNohJof0kXF068FyvGSk',
+  access_token: '11933972-UNLyz89vetxd8Qpf13TtyGagFMLtH0Yv6hDmAd405',
+  access_token_secret: 'ML32Ip73YbRYODCcJnhoR1GTJGvKIpayT9BuO5TzV9xxN'
+};
+
+module.exports = twitterKeys;
